964f2d98dd2c88eb52a7cafa8fdbc95b1204cd92
Rahul Kumar <rahuleigle@gmail.com>
2025-01-14 16:49:43 +0530

    adds build.yml

--- src/test/java/com/rawrahul/journalApp/service/UserArgumentsProvider.java
+++ src/test/java/com/rawrahul/journalApp/service/UserArgumentsProvider.java
@@ -1,18 +1,18 @@
-package com.rawrahul.journalApp.service;
-
-import com.rawrahul.journalApp.entity.User;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-
-import java.util.stream.Stream;
-
-public class UserArgumentsProvider implements ArgumentsProvider {
-    @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-        return Stream.of(
-                Arguments.of(User.builder().username("ram").password("ram").build()),
-                Arguments.of(User.builder().username("sita").password("sita").build())
-                );
-    }
-}
+//package com.rawrahul.journalApp.service;
+//
+//import com.rawrahul.journalApp.entity.User;
+//import org.junit.jupiter.api.extension.ExtensionContext;
+//import org.junit.jupiter.params.provider.Arguments;
+//import org.junit.jupiter.params.provider.ArgumentsProvider;
+//
+//import java.util.stream.Stream;
+//
+//public class UserArgumentsProvider implements ArgumentsProvider {
+//    @Override
+//    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+//        return Stream.of(
+//                Arguments.of(User.builder().username("ram").password("ram").build()),
+//                Arguments.of(User.builder().username("sita").password("sita").build())
+//                );
+//    }
+//}
--- src/test/java/com/rawrahul/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/rawrahul/journalApp/service/UserDetailsServiceImplTests.java
@@ -1,60 +1,60 @@
-package com.rawrahul.journalApp.service;
-
-import com.rawrahul.journalApp.entity.User;
-import com.rawrahul.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.mockito.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.*;
-
-
-// first method
-//@SpringBootTest
+//package com.rawrahul.journalApp.service;
+//
+//import com.rawrahul.journalApp.entity.User;
+//import com.rawrahul.journalApp.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//
+//import static org.mockito.Mockito.*;
+//
+//
+//// first method
+////@SpringBootTest
+////public class UserDetailsServiceImplTests {
+////
+////    @Autowired
+////    private UserDetailsServiceImpl userDetailsService;
+////
+////    @MockBean
+////    private UserRepository userRepository;
+////
+////    @Test
+////    public void loadUserByUsernameTest(){
+////        when(userRepository.findByusername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("nehru").password("jinna").roles(new ArrayList<>()).build());
+////        UserDetails user = userDetailsService.loadUserByUsername("rahul");
+////        Assertions.assertNotNull(user);
+////    }
+////
+////}
+//
+//// second method
 //public class UserDetailsServiceImplTests {
 //
-//    @Autowired
+//    @InjectMocks
 //    private UserDetailsServiceImpl userDetailsService;
 //
-//    @MockBean
+//    @Mock
 //    private UserRepository userRepository;
 //
+////    @BeforeEach
+////    void setUp(){
+////        MockitoAnnotations.initMocks(this);
+////    }
+//
+//    @Disabled
 //    @Test
 //    public void loadUserByUsernameTest(){
 //        when(userRepository.findByusername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("nehru").password("jinna").roles(new ArrayList<>()).build());
 //        UserDetails user = userDetailsService.loadUserByUsername("rahul");
 //        Assertions.assertNotNull(user);
 //    }
-//
 //}
-
-// second method
-public class UserDetailsServiceImplTests {
-
-    @InjectMocks
-    private UserDetailsServiceImpl userDetailsService;
-
-    @Mock
-    private UserRepository userRepository;
-
-//    @BeforeEach
-//    void setUp(){
-//        MockitoAnnotations.initMocks(this);
-//    }
-
-    @Disabled
-    @Test
-    public void loadUserByUsernameTest(){
-        when(userRepository.findByusername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("nehru").password("jinna").roles(new ArrayList<>()).build());
-        UserDetails user = userDetailsService.loadUserByUsername("rahul");
-        Assertions.assertNotNull(user);
-    }
-}
